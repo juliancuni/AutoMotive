c4fb706839b080aec9e5f8ab0ac1154412eb6123
Julian Cuni <juliancuni@gmail.com>
2019-08-15 20:32:39 +0200

    Added FrontEnd

---
+++ FrontEnd/src/app/layout/header/header.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+const screenfull = require('screenfull');
+
+import { UserblockService } from '../sidebar/userblock/userblock.service';
+import { SettingsService } from '../../core/settings/settings.service';
+import { MenuService } from '../../core/menu/menu.service';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+    navCollapsed = true; // for horizontal layout
+    menuItems = []; // for horizontal layout
+
+    isNavSearchVisible: boolean;
+    @ViewChild('fsbutton', { static: true }) fsbutton;  // the fullscreen button
+
+    constructor(public menu: MenuService, public userblockService: UserblockService, public settings: SettingsService) {
+
+        // show only a few items on demo
+        this.menuItems = menu.getMenu().slice(0, 4); // for horizontal layout
+
+    }
+
+    ngOnInit() {
+        this.isNavSearchVisible = false;
+
+        var ua = window.navigator.userAgent;
+        if (ua.indexOf("MSIE ") > 0 || !!ua.match(/Trident.*rv\:11\./)) { // Not supported under IE
+            this.fsbutton.nativeElement.style.display = 'none';
+        }
+
+        // Switch fullscreen icon indicator
+        const el = this.fsbutton.nativeElement.firstElementChild;
+        screenfull.on('change', () => {
+            if (el)
+                el.className = screenfull.isFullscreen ? 'fa fa-compress' : 'fa fa-expand';
+        });
+    }
+
+    toggleUserBlock(event) {
+        event.preventDefault();
+        this.userblockService.toggleVisibility();
+    }
+
+    openNavSearch(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setNavSearchVisible(true);
+    }
+
+    setNavSearchVisible(stat: boolean) {
+        // console.log(stat);
+        this.isNavSearchVisible = stat;
+    }
+
+    getNavSearchVisible() {
+        return this.isNavSearchVisible;
+    }
+
+    toggleOffsidebar() {
+        this.settings.toggleLayoutSetting('offsidebarOpen');
+    }
+
+    toggleCollapsedSideabar() {
+        this.settings.toggleLayoutSetting('isCollapsed');
+    }
+
+    isCollapsedText() {
+        return this.settings.getLayoutSetting('isCollapsedText');
+    }
+
+    toggleFullScreen(event) {
+        if (screenfull.enabled) {
+            screenfull.toggle();
+        }
+    }
+}
