206fbd3446c215e2adab38543fbcf343cc593584
Julian Cuni <juliancuni@gmail.com>
2019-08-20 02:19:27 +0200

    FrontEnd UserProfile Form (NotCompleted) & Style fixes

--- FrontEnd/src/app/routes/personal/profile/profile.component.ts
+++ FrontEnd/src/app/routes/personal/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AmUser, AmUserApi } from 'src/app/shared/sdk';
-// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import { CustomValidators } from 'ng2-validation';
 import { ToastModel } from 'src/app/shared/msInterfaces/interfaces';
 import { MsToasterService } from 'src/app/shared/services/mstoaster.service';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
     selector: 'app-profile',
@@ -12,8 +15,8 @@ import { MsToasterService } from 'src/app/shared/services/mstoaster.service';
 })
 export class ProfileComponent implements OnInit {
 
+    private userDataForm: FormGroup;
     private user: AmUser;
-    // private userDataForm: FormGroup;
     private loading: boolean = false;
     private toast: ToastModel;
     private emailError: boolean = false;
@@ -21,6 +24,16 @@ export class ProfileComponent implements OnInit {
     private usernameError: boolean = false;
     private error: {};
 
+    private editEmer: boolean = false;
+    private editMbiemer: boolean = false;
+    private editUsername: boolean = false;
+    private editEmail: boolean = false;
+    private editAdresa: boolean = false;
+    private editTelefon: boolean = false;
+    private editDatelindja: boolean = false;
+    private editPassword: boolean = false;
+
+    private subscriptions: Subscription[] = new Array<Subscription>();
 
     bsValue = new Date();
     bsConfig = {
@@ -31,8 +44,8 @@ export class ProfileComponent implements OnInit {
 
     constructor(
         private _amUser: AmUserApi,
-        // private fb: FormBuilder,
         private _msToasterService: MsToasterService,
+        private _fb: FormBuilder,
     ) {
 
     }
@@ -45,27 +58,90 @@ export class ProfileComponent implements OnInit {
         // if (this.userDataForm.valid) { }
     }
 
-    ngOnInit() {
-        this._amUser.getCurrent().subscribe((res: AmUser) => {
-            this.user = res;
-            
-        }, (err) => {
-
-        }, () => {
-            // this.initUserDataForm();
-        })
+    enableEdit(field: string): void {
+        this["edit" + field] ? this["edit" + field] = false : this["edit" + field] = true;
+        this["edit" + field] ? this.userDataForm.get(field.toLowerCase()).enable() : this.userDataForm.get(field.toLowerCase()).disable();
+        this.userDataForm.get(field.toLowerCase()).reset(this.user[field.toLowerCase()]);
     }
 
-    initUserDataForm() {
-        // this.userDataForm = this.fb.group({
-        //     'emer': [{ value: this.user.emer, disabled: false }, [Validators.required]],
-        //     'mbiemer': [null, Validators.required],
-        //     'datelindja': [null, null],
-        //     'username': [null, Validators.required],
-        //     'email': [null, Validators.compose([Validators.required, CustomValidators.email])],
-        //     'telefon': [null, null],
-        //     'adresa': [null, null],
-        //     'password': [null, Validators.required],
-        // });
+    saveField(field: string, value: string) {
+        let key = field.toLowerCase();
+
+        let minErr: boolean = this.userDataForm.controls[key].hasError('minlength');
+        let reqErr: boolean = this.userDataForm.controls[key].hasError('required');
+        console.log(key + " min: " + minErr, key + " req: " + reqErr)
+        if (minErr || reqErr) {
+            this.enableEdit(field);
+        } else {
+            this.loading = true
+            //Clone Amuser Obj
+            let updatedAmUser: AmUser = { ...this.user };
+            updatedAmUser[key] = value;
+            if (key === "newpassword") {
+
+                //Ketu kemi nje problem me fjalekalimet. Rregulloje
+
+                let oldPassword: string = this.userDataForm.get('password').value;
+                let newPassword: string = this.userDataForm.get('newpass').value;
+                this._amUser.changePassword(oldPassword, newPassword).subscribe((res) => {
+                    this.enableEdit(field);
+                    this.loading = false;
+                }, (err) => {
+                    this.enableEdit(field);
+                    this.loading = false;
+                })
+            } else if (this.user[key] === value) {
+                this.enableEdit(field);
+                this.loading = false;
+            } else {
+                this._amUser.upsertPatch(updatedAmUser).subscribe((res: AmUser) => {
+                    this.user = res;
+                }, (err) => {
+                    this.enableEdit(field);
+                    this.loading = false;
+                }, () => {
+                    this.enableEdit(field);
+                    this.loading = false;
+                    localStorage.setItem("UserPersonalData", JSON.stringify(this.user));
+                })
+            }
+        }
+    }
+
+    ngOnInit() {
+        this.userDataForm = this._fb.group({
+            'emer': [null, Validators.required],
+            'mbiemer': [null, Validators.required],
+            'username': [null, Validators.required],
+            'email': [null, Validators.compose([Validators.required, CustomValidators.email])],
+            'adresa': [null, null],
+            'telefon': [null, null],
+            'datelindja': [null, null],
+            'password': [null, Validators.required],
+            'newpass': [null, [Validators.required, Validators.minLength(6)]],
+        });
+        this.subscriptions.push(
+            this._amUser.getCurrent().subscribe((res: AmUser) => {
+                this.user = res;
+                this.userDataForm.get('emer').disable();
+                this.userDataForm.get('mbiemer').disable();
+                this.userDataForm.get('username').disable();
+                this.userDataForm.get('email').disable();
+                this.userDataForm.get('adresa').disable();
+                this.userDataForm.get('telefon').disable();
+                this.userDataForm.get('datelindja').disable();
+                this.userDataForm.get('password').disable();
+                this.userDataForm.get('emer').setValue(res.emer);
+                this.userDataForm.get('mbiemer').setValue(res.mbiemer)
+                this.userDataForm.get('username').setValue(res.username)
+                this.userDataForm.get('email').setValue(res.email)
+                this.userDataForm.get('adresa').setValue(res.adresa)
+                this.userDataForm.get('telefon').setValue(res.telefon)
+                this.userDataForm.get('datelindja').setValue(res.datelindja)
+            }, (err) => {
+
+            }, () => {
+            })
+        )
     }
 }
